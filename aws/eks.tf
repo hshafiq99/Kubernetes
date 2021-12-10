c11b94bb48bd843e7ac576d9f810fb0dea5e943a
hshafiq99 <hshafiq99@gmail.com>
2021-12-11 02:07:18 +0500

    Added Jenkins File

--- aws/eks.tf
+++ aws/eks.tf
@@ -2,6 +2,14 @@
 #EKS Cluster
 #############
 
+data "http" "workstation-external-ip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+locals {
+  workstation-external-cidr = "${chomp(data.http.workstation-external-ip.body)}/32"
+}
+
 resource "aws_iam_role" "cluster-role" {
   name = "cluster-role"
 

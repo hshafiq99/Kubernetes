c1bf53f9e002360f973ff3a60f2dd2c68f6dd334
hshafiq99 <hshafiq99@gmail.com>
2021-12-11 14:30:14 +0500

    Enabled Autoscaling

--- aws/autoscaling.tf
+++ aws/autoscaling.tf
@@ -1,17 +1,17 @@
 resource "aws_iam_role_policy_attachment" "workers_autoscaling" {
-  policy_arn = aws_iam_policy.worker_autoscaling.arn
-  role       = aws_eks_cluster.cluster.worker_iam_role_name
+  policy_arn = aws_iam_policy.worker_autoscaling_policy.arn
+  role       = aws_iam_role.cluster-role.name
 }
 
-resource "aws_iam_policy" "worker_autoscaling" {
-  name_prefix = "eks-worker-autoscaling-${aws_eks_cluster.cluster.cluster_id}"
-  description = "EKS worker node autoscaling policy for cluster ${aws_eks_cluster.cluster.cluster_id}"
-  policy      = data.aws_iam_policy_document.worker_autoscaling.json
+resource "aws_iam_policy" "worker_autoscaling_policy" {
+  name_prefix = "eks-worker-autoscaling-${var.cluster_name}"
+  description = "EKS worker node autoscaling policy for cluster ${var.cluster_name}"
+  policy      = data.aws_iam_policy_document.worker_autoscaling_policy_document.json
   path        = var.iam_path
   tags        = var.tags
 }
 
-data "aws_iam_policy_document" "worker_autoscaling" {
+data "aws_iam_policy_document" "worker_autoscaling_policy_document" {
   statement {
     sid    = "eksWorkerAutoscalingAll"
     effect = "Allow"
